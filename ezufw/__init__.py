3ba189da624ad85c746516bb46115f92f92fafd5
marek <Marek Gancarz>
2021-08-11 22:39:25 +0200

    [0.0.9] modified __init__ for proper imports

--- ezufw/__init__.py
+++ ezufw/__init__.py
@@ -1,1 +1,19 @@
-from .ezufw import *
+from .ezufw import (
+    rules,
+    enable,
+    disable,
+    status,
+    execute,
+    reset,
+
+    delete_by_port,
+    delete_by_ip,
+
+    deny,
+    denyA,
+    deny_from,
+
+    allow_from,
+
+    insert
+)
